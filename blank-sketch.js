2fa5de8f69a1510b5155b742d0ee617b518c1ab3
sydney <sydney@carbon.lan>
2024-06-04 19:59:34 -0700

    add a blank p5js sketch

---
+++ blank-sketch.js
@@ -0,0 +1,17 @@
+let frame = 0;
+let bgColourValue = 0;
+
+
+function setup() {
+  createCanvas(800, 600);
+
+
+
+  frame++;
+}
+
+
+function draw() {
+  background(bgColourValue);
+  translate(width / 2, height / 2);
+}
