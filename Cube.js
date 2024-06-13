c15d3ca5b449cdebf90ae8ec06fd44aa5c091613
sydney <sydney@carbon.lan>
2024-06-12 18:51:41 -0700

    fix Cube faces so it works with back face culling. Save the original as BadCube (good for demoing)

--- Cube.js
+++ Cube.js
@@ -3,29 +3,32 @@ class Cube extends RenderObject {
     
     const points = () => {
       const points = [];
-      points.push(createVector(-.5, -.5, -.5));
+      points.push(createVector(.5, -.5, .5));
       points.push(createVector(.5, -.5, -.5));
       points.push(createVector(.5, .5, -.5));
+      points.push(createVector(.5, .5, .5));
+      points.push(createVector(-.5, .5, .5));
       points.push(createVector(-.5, .5, -.5));
-      points.push(createVector(-.5, -.5, 0.5));
-      points.push(createVector(.5, -.5, 0.5));
-      points.push(createVector(.5, .5, 0.5));
-      points.push(createVector(-.5, .5, 0.5));
+      points.push(createVector(-.5, -.5, -.5));
+      points.push(createVector(-.5, -.5, .5));
+
       return points;
     }
 
-    const triangles = [[0, 1, 2],
-    [0, 2, 3],
-    [4, 5, 6],
-    [4, 6, 7],
-    [0, 4, 5],
-    [0, 1, 5],
-    [1, 5, 6],
-    [1, 2, 6],
-    [2, 6, 7],
-    [2, 3, 7],
-    [3, 7, 4],
-    [3, 0, 4]];
+    const triangles = [
+      [0, 1, 2], // good
+      [0, 2, 3], // good
+      [4, 5, 6], // good
+      [4, 6, 7], // good
+      [3, 4, 7], // fixed
+      [3, 7, 0], // fixed
+      [7, 6, 1], // fixed
+      [7, 1, 0], // fixed
+      [3, 2, 5], // fixed 
+      [3, 5, 4], // fixed
+      [1, 6, 5], // fixed
+      [1, 5, 2]  // fixed
+    ];
 
     super(points(), triangles, position, angleX, angleY, angleZ, colour);
   }
